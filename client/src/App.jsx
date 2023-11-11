0cf29791162d868329776a3c938d80f7b038b550
João Marcelo Dantas <jmgrd98@gmail.com>
2023-11-11 14:24:42 -0300

    routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,9 @@ const App = () => {
 
         <Routes>
           <Route path='/' element={<Home/>}/>
-
+          <Route path='/create-campaign' element={<CreateCampaign/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+          <Route path='/campaign-details/:id' element={<CampaignDetails/>}/>
         </Routes>
       </div>
     </div>
