c254c4e8840cde42acfe26ee3f27f059856862fd
João Marcelo Dantas <jmgrd98@gmail.com>
2023-11-11 10:53:32 -0300

    Update Crowdfunding.sol

--- web3/contracts/Crowdfunding.sol
+++ web3/contracts/Crowdfunding.sol
@@ -23,17 +23,48 @@ contract Crowdfunding {
     uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
         
         Campaign storage campaign = campaigns[numberOfCampaigns];
+        require(campaign.deadline < block.timestamp, "The deadline should be a date in the future.");
+
+        campaing.owner = _owner;
+        campaing.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+
+        numberOfCampaigns++;
+
+        return numberOfCampaigns - 1;
     }
 
-    function donateToCampaign() {
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
 
+        Campaign storage campaign = campaigns[_id];
+
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        (bool sent,) = payable(campaign.owner).call{value: amount}("");
+
+        if (sent) {
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
     }
 
-    function getDonators() {
-
+    function getDonators(uint256 _id) view public returns(address[] memory, uint256[] memory){
+        return (campaigns[_id].donators, campaigns[_id].donations);
     }
 
-    function getCampaigns() {
+    function getCampaigns() view public returns(Campaign[] memory) {
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
 
+        for (uint i = 0; i < numberOfCampaigns; i++) {
+            Campaign.storage item = campaigns[i];
+            allCampaigns[i] = item;
+        }
+
+        return allCampaigns;
     }
 }
