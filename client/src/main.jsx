72680eb6f876faa20d565772d1814757ef8a18f8
João Marcelo Dantas <jmgrd98@gmail.com>
2023-11-11 12:23:41 -0300

    adding tailwind

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from "react-dom/client";
 import {BrowserRouter} from 'react-router-dom';
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <ThirdwebProvider desiredChainId={ChainId.Sepolia}>
-        <App/>
+    <ThirdwebProvider desiredChainId={ChainId.Goerli}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
     </ThirdwebProvider>
 )
