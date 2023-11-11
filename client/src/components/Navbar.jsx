ba9654bc634014acadd5b9134c0e990d2a0038e9
João Marcelo Dantas <jmgrd98@gmail.com>
2023-11-11 14:13:25 -0300

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -30,8 +30,44 @@ const Navbar = () => {
         else 'connect()'
       }}
       />
-    </div>
 
+      <Link to='/profile'>
+        <div className='w-[52px] h-[52px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer'>
+          <img src={thirdweb} alt="user" className='w-[60%] h-[60%] object-contain'/>
+        </div>
+      </Link>
+    </div>
+      
+      <div className='sm:hidden flex justify-between items-center relative'>
+      <div className='w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer'>
+          <img src={thirdweb} alt="user" className='w-[60%] h-[60%] object-contain'/>
+        </div>
+
+        <img
+        src={menu}
+        alt='menu'
+        className='w-[34px] h-[34px] object-contain cursor-pointer'
+        onClick={() => setToggleDrawer(!toggleDrawer)}
+        />
+
+        <div className={`absolute top-[60px] right-0 left-0 bg-[#1c1c24] z-10 shadow-secondary py-4 ${!toggleDrawer ? 'translate-y-[100vh]' : 'translate-y-0'} transition-all duration-700`}>
+          <ul className='mb-4'>
+            {navlinks.map((link) => {
+              <li
+              key={link.name}
+              className={`flex p-4 ${isActive === link.name && 'bg-[#3a3a43]'}`}
+              onClick={() => {
+                setIsActive(link.name);
+                setToggleDrawer(false);
+                navigate(link.link);
+              }}
+              >
+                <img src={link.imgUrl}  />
+              </li>
+            })}
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
