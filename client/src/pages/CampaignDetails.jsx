2844a6903c0282031ed7a0d8775f1f310bc5a4ae
João Marcelo Dantas <jmgrd98@gmail.com>
2023-11-11 21:06:29 -0300

    CountBox component

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -2,13 +2,13 @@ import React, {useState, useEffect} from 'react';
 import {useLocation} from 'react-router-dom';
 import {ethers} from 'ethers';
 import {useStateContext} from '../context';
-import {CustomButton} from '../components';
+import {CustomButton, CountBox} from '../components';
 import {calculateBarPercentage, daysLeft} from '../utils';
 import {thirdweb} from '../assets';
 
 const CampaignDetails = () => {
 
-  const {state} = useLocation;
+  const {state} = useLocation();
   const {getDonations, contract, address} = useStateContext();
   const [isLoading, setIsLoading] = useState(false);
   const [amount, setAmount] = useState('');
@@ -16,21 +16,28 @@ const CampaignDetails = () => {
 
   const remainingDays = daysLeft(state.deadline);
 
+  console.log(state);
+
   return (
     <div>
       {isLoading && 'Loading...'}
 
-      <div className='w-full flex md:flex-row flex-col mt-10 gap-[30´px]'>
+      <div className='w-full flex md:flex-row flex-col mt-10 gap-[30px]'>
         <div className="flex-1 flex-col">
           <img src={state.image} alt="campaign" className="w-full h-[410px] object-cover rounded-xl" />
 
 
           <div className="relative w-full h-[5px] bg-[#3a3a43] mt-2">
             <div style={{width: `${calculateBarPercentage(state.target, state.amountCollected)}%`, maxWidth: '100%'}} className="absolute h-full bg-[#4acd8d]">
-
             </div>
-
           </div>
+          </div>
+
+          <div className="flex md:w-[150px] w-full flex-wrap justify-between gap-[30px]">
+            <CountBox title="Days Left" value={remainingDays} />
+            <CountBox title={`Raised of ${state.target}`} value={state.amountCollected} />
+            <CountBox title="Total Backers" value={donators.length} />
+
           </div>
 
       </div>
